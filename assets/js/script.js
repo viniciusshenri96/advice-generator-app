238fe8956c8d8a6266ada097ab2bf0557deb9285
Vinicius <vinicius_henrique96@hotmail.com>
2022-12-05 20:36:10 -0300

    :sparkles: feat: Finished implementing advice update with async/await

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const adviceText = document.querySelector(".advice__text");
+const adviceID = document.querySelector(".advice__id-number");
 const btnUpdate = document.querySelector(".btn__generate");
+console.log(adviceID);
 
-const updateAdvice = async function () {
-  const res = await fetch("https://api.adviceslip.com/advice");
-  const data = await res.json();
-
-  console.log(data);
-  btnUpdate.addEventListener("click", function () {
-    adviceText.textContent = data.slip.advice;
-  });
-};
-const data = updateAdvice();
+btnUpdate.addEventListener("click", async () => {
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    adviceText.innerText = `“${data.slip.advice}”`;
+    adviceID.innerText = data.slip.id;
+    console.log(adviceID.innerText);
+  } catch (err) {
+    console.error(new Error("It is not an internet connection"));
+  }
+});

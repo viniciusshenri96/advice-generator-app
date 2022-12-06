b993ff641f864ac3cb6ab7471b929f4842c5502d
Vinicius <vinicius_henrique96@hotmail.com>
2022-12-06 20:38:10 -0300

    :recycle: refactor: refactored project

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@
 const adviceText = document.querySelector(".advice__text");
 const adviceID = document.querySelector(".advice__id-number");
 const btnUpdate = document.querySelector(".btn__generate");
-console.log(adviceID);
 
 btnUpdate.addEventListener("click", async () => {
   try {
@@ -11,7 +10,6 @@ btnUpdate.addEventListener("click", async () => {
     const data = await res.json();
     adviceText.innerText = `“${data.slip.advice}”`;
     adviceID.innerText = data.slip.id;
-    console.log(adviceID.innerText);
   } catch (err) {
     console.error(new Error("It is not an internet connection"));
   }
